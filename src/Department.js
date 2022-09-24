fdb90e2819f6fb122866ae2b3d48b11f36f08f18
fanik05 <fatinanik129@gmail.com>
2022-09-24 17:06:55 +0600

    api call fixed

--- src/Department.js
+++ src/Department.js
@@ -1,61 +1,63 @@
-import React, { Component } from 'react';
-import { Table, Button, ButtonToolbar } from 'react-bootstrap';
-import axios from 'axios';
+import React, { Component } from 'react'
+import { Table, Button, ButtonToolbar } from 'react-bootstrap'
 
 class Department extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { deps: [] };
-    }
-    refreshList() {
-        //calling department api by axios
-        // fetch('http://localhost:3000/department/list')
-        //     .then(response => response.json())
-        //     .then(data => {
-        //         this.setState({ deps: data });
-        //     });
-        //axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-        axios.get(`http://localhost:3000/department/list`,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                }
-            }
-        ).then(response => {
-            this.setState({ deps: response.data.deparments });
-        });
-    }
-    componentDidMount() {
-        this.refreshList();
-    }
-    componentDidUpdate() {
-        this.refreshList();
-        console.log(this.state.deps);
-    }
-    render() {
-        const { deps } = this.state;
-        return (
-            <div className='mt-5 d-flex justify-content-left'>
-                <Table className='mt-4' striped bordered hover size='sm'>
-                    <thead>
-                        <tr>
-                            <th>DepartmentId</th>
-                            <th>DepartmentName</th>
-                            <th>Options</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {deps.map(dep =>
-                            <tr key={dep.DepartmentId}>
-                                <td>{dep.DepartmentId}</td>
-                                <td>{dep.DepartmentName}</td>
-                                <td>Edit</td>
-                            </tr>
-                        )}
-                    </tbody>
-                </Table>
-            </div>
-        );
-    }
+  constructor(props) {
+    super(props)
+    this.state = { deps: [], error: null, isLoaded: false }
+  }
+  refreshList() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => response.json())
+      .then(
+        result => this.setState({ deps: result, isLoaded: true }),
+        error => this.setState({ isLoaded: true, error })
+      )
+  }
+  componentDidMount() {
+    this.refreshList()
+  }
+  componentDidUpdate() {
+    console.log(this.state.deps)
+  }
+  render() {
+    const { error, isLoaded, deps } = this.state
+    return (
+      <div className="mt-5 d-flex justify-content-left">
+        <Table className="mt-4" striped bordered hover size="sm">
+          <thead>
+            <tr>
+              <th>DepartmentId</th>
+              <th>DepartmentName</th>
+              <th>Options</th>
+            </tr>
+          </thead>
+          <tbody>
+            {error && (
+              <tr>
+                <td></td>
+                <td>Error: {error.message}</td>
+                <td></td>
+              </tr>
+            )}
+            {!isLoaded && (
+              <tr>
+                <td></td>
+                <td>Loading...</td>
+                <td></td>
+              </tr>
+            )}
+            {deps.map(dep => (
+              <tr key={dep.id}>
+                <td>{dep.userId}</td>
+                <td>{dep.title}</td>
+                <td>Edit</td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </div>
+    )
+  }
 }
-export default Department;
+export default Department
